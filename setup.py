095d8b412ea4b732682e696d2ca587c0547c1e30
Honglei <jhonglei@gmail.com>
2014-11-07 23:46:57 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='jquery-params',
+    version='1.0.0',
+    url='https://github.com/honglei/jquery_params/',
+    license='BSD',
+    author='Jiang Honglei',
+    author_email='jhonglei@gmail.com',
+    description='A python library to parse jquery.param() string.',
+    long_description=__doc__,
+    py_modules=['jquery_params'],
+    platforms='any',
+    install_requires=[],
+)
